efcbe85bbcaf9c80195a6b6effbe780d1beffeb6
Bilal Ahmad <pro123908@gmail.com>
2020-07-29 04:39:02 +0500

    Comments added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Animations in a absolute manner without any relation to each other
+
 // gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
 // gsap.from(".link", { duration: 1, opacity: 0, delay: 1, stagger: ".5s" });
 
@@ -16,13 +18,15 @@
 
 // gsap.to(obj, { x: 100, onUpdate: () => console.log(obj.x), duration: 2 });
 
+// Timeline provides relation between the animation,one is executed after the other is finished and is related to it
+// Default parameters can be passed to the timeline which will be common for all the animations in the timeline
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 
 timeline
   .from(".header", { y: "-100%", ease: "bounce" })
-  .from(".link", { opacity: 0, stagger: ".5s" })
-  .from(".right", { x: "-100vw", ease: "power2.in" }, 1)
-  .from(".left", { x: "-100%" }, "<.5")
+  .from(".link", { opacity: 0, stagger: ".5s" }) // stagger property allows you to execute animation on targeted elements in linear/asynchronous manner
+  .from(".right", { x: "-100vw", ease: "power2.in" }, 1) // 1 shows absolute delay when the timeline is started
+  .from(".left", { x: "-100%" }, "<.5") // "<.5 defines the offset with the last animation , in this case this animation will start after .5s second of the last animation is started"
   .to(".footer", { y: 0, ease: "elastic" })
   .fromTo(
     ".button",
@@ -30,6 +34,7 @@ timeline
     { opacity: 1, scale: 1, rotation: 0 }
   );
 
+// timeline gives you many features, it allows you to reverse the timeline and with the speed you want, in case it is reversed 3 times the speed of the animation
 const button = document.querySelector(".button");
 
 button.addEventListener("click", () => {
